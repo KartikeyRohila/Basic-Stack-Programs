f53f3651d55b37ff4de1d85fb4397173169f4399
Kartikey Rohila <kartikeyrohila@gmail.com>
2021-02-26 15:48:01 +0530

    Final Stack file

--- Stack_Infix_to_Postfix.cpp
+++ Stack_Infix_to_Postfix.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Function to check weather a given character is an operator or not
+//Return 1 if it is an operator else return 0
+int isoperator(char c)
+{
+    if (c=='^' || c=='+' || c=='-' || c=='*' || c=='/')
+        return 1;
+    return 0;
+}
+
 //Function to check the precedence of operators
 int prec (char c)
 {
@@ -17,52 +26,69 @@ int prec (char c)
 int main()
 {
     stack <char> st;    //Create an empty stack
-    long long int n;
+    long long int n,flag=0;
     string s,ans="";
     cin >> s;
     n=s.length();
-    
+
+    //Check weather expression is valid or not
     for (int i=0;i<n;i++)
     {
-        if (('a'<=s[i] && s[i]<='z') || ('A'<=s[i] && s[i]<='Z'))
+        if ( (isalpha(s[i])==0) && (isoperator(s[i])==0) && s[i]!=' ')
         {
-            ans+=s[i];
-        }
-        else if (s[i]=='(')
-        {
-            st.push('(');
-        }
-        else if (s[i]==')')
-        {
-            while (st.empty()==false && st.top()!='(')
-            {
-                char c=st.top();
-                st.pop();
-                ans+=c;
-            }
-            if (st.top()=='(')
-            {
-                st.pop();
-            }
-        }
-        else
-        {
-            while (st.empty()==false && prec(s[i])<=prec(st.top()))
-            {
-                char c=st.top();
-                st.pop();
-                ans+=c;
-            }
-            st.push(s[i]);
+            flag=1;
+            break;
         }
     }
 
-    while (st.empty()==false)
+    if (flag==1)
     {
-        char c=st.top();
-        st.pop();
-        ans+=c;
+        cout << "\nExpression is not valid\n";
     }
-    cout << ans <<"\n";
+    else
+    {
+        cout << "\nExpression is valid\n";
+        for (int i=0;i<n;i++)
+        {
+            if (('a'<=s[i] && s[i]<='z') || ('A'<=s[i] && s[i]<='Z'))
+            {
+                ans+=s[i];
+            }
+            else if (s[i]=='(')
+            {
+                st.push('(');
+            }
+            else if (s[i]==')')
+            {
+                while (st.empty()==false && st.top()!='(')
+                {
+                    char c=st.top();
+                    st.pop();
+                    ans+=c;
+                }
+                if (st.top()=='(')
+                {
+                    st.pop();
+                }
+            }
+            else
+            {
+                while (st.empty()==false && prec(s[i])<=prec(st.top()))
+                {
+                    char c=st.top();
+                    st.pop();
+                    ans+=c;
+                }
+                st.push(s[i]);
+            }
+        }
+        while (st.empty()==false)
+        {
+            char c=st.top();
+            st.pop();
+            ans+=c;
+        }
+        cout << ans <<"\n";
+    }
     return 0;
 }
--- Stack_Infix_to_Prefix.cpp
+++ Stack_Infix_to_Prefix.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Function to check weather a given character is an operator or not
+//Return 1 if it is an operator else return 0
+int isoperator(char c)
+{
+    if (c=='^' || c=='+' || c=='-' || c=='*' || c=='/')
+        return 1;
+    return 0;
+}
+
 //Function to check the precedence of operators
 int prec (char a)
 {
@@ -18,51 +27,68 @@ int main()
 {
     stack <char> st;    //Create an empty stack
     string s,ans="";
-    int n;
+    int n,flag=0;
     cin >> s;
     n=s.length();
 
-    for (int i=n-1;i>=0;i--)
+    for (int i=0;i<n;i++)
     {
-        if (('a'<=s[i] && s[i]<='z') || ('A'<=s[i] && s[i]<='Z'))
+        if ( (isalpha(s[i])==0) && (isoperator(s[i])==0) && s[i]!=' ')
         {
-            char c=s[i];
-            ans+=c;
-        }
-        else if (s[i]==')')
-            st.push(s[i]);
-        else if (s[i]=='(')
-        {
-            while (st.empty()==false && st.top()!=')')
-            {
-                char c=st.top();
-                st.pop();
-                ans+=c;
-            }
-            if (st.top()==')')
-            {
-                st.pop();
-            }
-        }
-        else
-        {
-            while (st.empty()==false && prec(s[i])<prec(st.top()))
-            {
-                char c=st.top();
-                ans+=c;
-                st.pop();
-            }
-            st.push(s[i]);
+            flag=1;
+            break;
         }
     }
 
-    while (st.empty()==false)
+    if (flag==1)
     {
-        char c=st.top();
-        st.pop();
-        ans+=c;
+        cout << "\nExpression is invalid\n";
     }
-    reverse(ans.begin(),ans.end());
-    cout << ans <<"\n";
+    else
+    {
+        cout << "\nExpression is valid\n";
+        for (int i=n-1;i>=0;i--)
+        {
+            if (('a'<=s[i] && s[i]<='z') || ('A'<=s[i] && s[i]<='Z'))
+            {
+                char c=s[i];
+                ans+=c;
+            }
+            else if (s[i]==')')
+                st.push(s[i]);
+            else if (s[i]=='(')
+            {
+                while (st.empty()==false && st.top()!=')')
+                {
+                    char c=st.top();
+                    st.pop();
+                    ans+=c;
+                }
+                if (st.top()==')')
+                {
+                    st.pop();
+                }
+            }
+            else
+            {
+                while (st.empty()==false && prec(s[i])<prec(st.top()))
+                {
+                    char c=st.top();
+                    ans+=c;
+                    st.pop();
+                }
+                st.push(s[i]);
+            }
+        }
+
+        while (st.empty()==false)
+        {
+            char c=st.top();
+            st.pop();
+            ans+=c;
+        }
+        reverse(ans.begin(),ans.end());
+        cout << ans <<"\n";
+    }
     return 0;
 }
